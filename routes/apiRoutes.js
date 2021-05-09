aa2f7af82ef1beb7b135d2f63246262e9b3567fa
Mike Matcke <mr.matcke@gmail.com>
2021-05-09 10:29:54 -0700

    updating files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const db = require("./models");
+const db = require("../models");
 
 //create new workout
 router.post("/api/workouts", ({ body }, res) => {
