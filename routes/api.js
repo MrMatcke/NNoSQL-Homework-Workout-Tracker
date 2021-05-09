c7b0111c450a4c5c770fa8f8baf80af2365823fa
Mike Matcke <mr.matcke@gmail.com>
2021-05-08 22:49:30 -0700

    updates

--- routes/api.js
+++ routes/api.js
@@ -45,17 +45,7 @@ module.exports = function(app) {
     });
 };
 
-
+module.exports = router;
 
 
 //HTML routes
-module.exports = function(app) {
-app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
-
-  app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
-
-};
