fd98eda685c78d5108ec88d24a98a6389034345e
Mike Matcke <mr.matcke@gmail.com>
2021-05-09 10:46:48 -0700

    bugfix

--- server.js
+++ server.js
@@ -18,8 +18,81 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
+const router = require("express").Router();
+const db = require("../models");
+
+//create new workout
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//add exercises
+router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body.duration);
+    db.Workout.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+            
+        },
+        {
+            new: true,
+            upsert: true
+        }
+    ).then(updatedWorkout => {
+        console.log(updatedWorkout);
+        res.json(updatedWorkout);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+const path = require("path");
+
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
+
+//app.use(require("./.git/"));
+//app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
