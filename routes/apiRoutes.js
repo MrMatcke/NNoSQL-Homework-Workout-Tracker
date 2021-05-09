c10f7d9b2e87beb6a7ad8310ace43f8d1df32885
Mike Matcke <mr.matcke@gmail.com>
2021-05-09 09:33:46 -0700

    debugging

---
+++ routes/apiRoutes.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const db = require("../Models");
+
+//create new workout
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//add exercises
+router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body.duration);
+    db.Workout.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+            
+        },
+        {
+            new: true,
+            upsert: true
+        }
+    ).then(updatedWorkout => {
+        console.log(updatedWorkout);
+        res.json(updatedWorkout);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
